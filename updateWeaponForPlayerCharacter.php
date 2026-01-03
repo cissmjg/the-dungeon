babd5faa6e902be72926a447777cefc49f8312ac
cissmjg <cissmjg@hotmail.com>
2026-01-03 17:43:55 -0500

    Moved meleeSpec1DamageBonus.php to /webio/. Updated corresponding references

--- updateWeaponForPlayerCharacter.php
+++ updateWeaponForPlayerCharacter.php
@@ -32,7 +32,7 @@ require_once __DIR__ . '/webio/meleeNumberOfHands.php';
 require_once __DIR__ . '/webio/meleeAdditionalText.php';
 require_once __DIR__ . '/webio/meleeHitBonus.php';
 require_once __DIR__ . '/webio/meleeDamageBonus.php';
-require_once 'meleeSpec1HitBonus.php';
+require_once __DIR__ . '/webio/meleeSpec1HitBonus.php';
 require_once 'meleeSpec1DamageBonus.php';
 require_once 'meleeSpec1Description.php';
 require_once 'meleeSpec2HitBonus.php';
