e7f9828936535ad4320fd6fe3cd2d086e712fcab
cissmjg <cissmjg@hotmail.com>
2026-01-03 22:08:28 -0500

    Remove unused variable spellPoolId

--- characterActionRouter.php
+++ characterActionRouter.php
@@ -20,7 +20,6 @@ require_once __DIR__ . '/webio/pageAction.php';
 require_once __DIR__ . '/webio/spellCatalogId.php';
 require_once __DIR__ . '/webio/spellSlotId.php';
 require_once __DIR__ . '/webio/spellLevel.php';
-require_once 'spellPoolId.php';
 require_once 'spellPoolSlotId.php';
 require_once __DIR__ . '/webio/spellDuration.php';
 require_once __DIR__ . '/webio/spellCastingTime.php';
