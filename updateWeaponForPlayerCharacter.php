64d615f7a2e66d6da936e0ed9eca6fadcb69e0f8
cissmjg <cissmjg@hotmail.com>
2026-01-03 20:21:01 -0500

    Moved missileSpec2Description.php to /webio/. Updated corresponding references

--- updateWeaponForPlayerCharacter.php
+++ updateWeaponForPlayerCharacter.php
@@ -51,7 +51,7 @@ require_once __DIR__ . '/webio/missileSpec1DamageBonus.php';
 require_once __DIR__ . '/webio/missileSpec1Description.php';
 require_once __DIR__ . '/webio/missileSpec2HitBonus.php';
 require_once __DIR__ . '/webio/missileSpec2DamageBonus.php';
-require_once 'missileSpec2Description.php';
+require_once __DIR__ . '/webio/missileSpec2Description.php';
 require_once 'missileSpec3HitBonus.php';
 require_once 'missileSpec3DamageBonus.php';
 require_once 'missileSpec3Description.php';
