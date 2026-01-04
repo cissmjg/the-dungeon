607c5683bb0d48bc6bb966a71beff748afaf1263
cissmjg <cissmjg@hotmail.com>
2026-01-04 12:07:29 -0500

    Removed generic web variables. Replaced with corresponding web variables containing the prefix melee and the prefix missile

--- weaponMediumRange.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once __DIR__ . '/webio/requiredParameter.php';
-
-function getWeaponMediumRange(&$errors, &$input, $default_value) {
-	getOptionalStringParameter($errors, $input, __FILE__, 'weaponMediumRange', $default_value);
-}
--- weaponShortRange.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once __DIR__ . '/webio/requiredParameter.php';
-
-function getWeaponShortRange(&$errors, &$input, $default_value) {
-	getOptionalStringParameter($errors, $input, __FILE__, 'weaponShortRange', $default_value);
-}
