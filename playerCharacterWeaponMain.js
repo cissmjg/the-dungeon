0da9c5862cc50593fdc95476af95669811ccefbb
cissmjg <cissmjg@hotmail.com>
2026-01-03 21:51:54 -0500

    Removed incorrect usage of TEXT_INPUT constant

--- playerCharacterWeaponMain.js
+++ playerCharacterWeaponMain.js
@@ -6,7 +6,7 @@ export function populateWeaponList(weaponListName, weaponSearchTextboxName) {
     const weaponQueryPattern = '#' + weaponSearchTextboxName;
     const weaponList = "#" + weaponListName;
     let weaponQueryAPI = buildURL('getWeaponProficiencyByPattern');
-    weaponQueryAPI = addParameter(weaponQueryAPI, TEXT_INPUT, $(weaponQueryPattern).val());
+    weaponQueryAPI = addParameter(weaponQueryAPI, 'textInput', $(weaponQueryPattern).val());
     $(weaponList).empty();
     $.getJSON(weaponQueryAPI,
         function(data, textStatus, jqXHR) {
