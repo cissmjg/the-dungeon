e373af9170d9b06df28b92a2a95b7652eeb4db30
cissmjg <cissmjg@hotmail.com>
2026-01-04 12:03:46 -0500

    Remove weaponDamage.php. Replaced with meleeWeaponDamage and missileWeaponDamage

--- weaponDamage.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once __DIR__ . '/webio/requiredParameter.php';
-
-function getWeaponDamage(&$errors, &$input) {
-	getRequiredStringParameter($errors, $input, __FILE__, 'weaponDamage');
-}
