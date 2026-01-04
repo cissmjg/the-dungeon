ca5cec7908fb256676df952671db5f7df9f12db6
cissmjg <cissmjg@hotmail.com>
2026-01-04 11:32:32 -0500

    Updated 'playerCharacterWeaponSkillId' to PLAYER_CHARACTER_WEAPON_SKILL_ID constant. Moved playerCharacterWeaponSkillId.php to /webio/.

--- webio/playerCharacterWeaponSkillId.php
+++ webio/playerCharacterWeaponSkillId.php
@@ -2,11 +2,12 @@
 require_once __DIR__ . '/webio/requiredParameter.php';
 require_once __DIR__ . '/webio/optionalParameter.php';
 require_once __DIR__ . '/helper/WebParameterHelper.php';
+const PLAYER_CHARACTER_WEAPON_SKILL_ID = 'playerCharacterWeaponSkillId';
 
 function getPlayerCharacterWeaponSkillId(&$errors, &$input, $default_value) {
-	getRequiredIntegerParameter($errors, $input, __FILE__, 'playerCharacterWeaponSkillId');
+	getRequiredIntegerParameter($errors, $input, __FILE__, PLAYER_CHARACTER_WEAPON_SKILL_ID);
 }
 
 function getOptionalPlayerCharacterWeaponSkillId(&$errors, &$input, $default_value) {
-	getOptionalIntegerParameter($errors, $input, __FILE__, 'playerCharacterWeaponSkillId', OPTIONAL_INTEGER_PARAMETER);
+	getOptionalIntegerParameter($errors, $input, __FILE__, PLAYER_CHARACTER_WEAPON_SKILL_ID, OPTIONAL_INTEGER_PARAMETER);
 }
