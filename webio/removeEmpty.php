36ed7dea0f61b90479d72c9e4e5ac8307ef88ef9
cissmjg <cissmjg@hotmail.com>
2026-01-05 16:14:12 -0500

    Corrected name of REMOVE_EMPTY constant

--- webio/removeEmpty.php
+++ webio/removeEmpty.php
@@ -1,7 +1,7 @@
 <?php
 require_once __DIR__ . '/requiredParameter.php';
 require_once __DIR__ . '/optionalParameter.php';
-const CAST_STATUS = 'removeEmpty';
+const REMOVE_EMPTY = 'removeEmpty';
 
 function getRemoveEmpty(&$errors, &$input) {
     getOptionalBooleanParameter($errors, $input, __FILE__, REMOVE_EMPTY, false);
