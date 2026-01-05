3fb152a064d6482441290a47e1a152da4633e8ee
cissmjg <cissmjg@hotmail.com>
2026-01-05 12:32:28 -0500

    Corrected incorrect constant assignment

--- webio/characterLevel.php
+++ webio/characterLevel.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__ . '/requiredParameter.php';
-const CHARACTER_LEVEL = CHARACTER_LEVEL;
+const CHARACTER_LEVEL = 'characterLevel';
 
 function getCharacterLevel(&$errors, &$input) {
 	getRequiredIntegerParameter($errors, $input, __FILE__, CHARACTER_LEVEL);
