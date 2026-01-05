43b1839026f23dc222fb8c909bf6eaab24ee9264
cissmjg <cissmjg@hotmail.com>
2026-01-05 14:33:14 -0500

    Updated incorrect constant assignment

--- webio/isReady.php
+++ webio/isReady.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__ . '/requiredParameter.php';
-const IS_READY = IS_READY;
+const IS_READY = 'isReady';
 
 function getIsReady(&$errors, &$input) {
 	getRequiredStringParameter($errors, $input, __FILE__, IS_READY);
