b731832b6f0f7d3e27e2d36d562d99cd3456f17a
cissmjg <cissmjg@hotmail.com>
2026-01-05 14:31:27 -0500

    Updated incorrect constant assignment

--- webio/spellTypeId.php
+++ webio/spellTypeId.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__ . '/requiredParameter.php';
-const SPELL_TYPE_ID = SPELL_TYPE_ID;
+const SPELL_TYPE_ID = 'spellRTypeId';
 
 function getSpellTypeId(&$errors, &$input) {
 	getRequiredIntegerParameter($errors, $input, __FILE__, SPELL_TYPE_ID);
