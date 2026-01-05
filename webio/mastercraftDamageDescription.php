83638606359e1977b4fc353d97d0e96e892e0124
cissmjg <cissmjg@hotmail.com>
2026-01-05 14:34:48 -0500

    Updated incorrect constant assignment

--- webio/mastercraftDamageDescription.php
+++ webio/mastercraftDamageDescription.php
@@ -2,7 +2,7 @@
 require_once __DIR__ . '/requiredParameter.php';
 require_once __DIR__ . '/optionalParameter.php';
 require_once __DIR__ . '/../helper/WebParameterHelper.php';
-const MASTERCRAFT_DAMAGE_DESCRIPTION = MASTERCRAFT_DAMAGE_DESCRIPTION;
+const MASTERCRAFT_DAMAGE_DESCRIPTION = 'mastercraftDamageDescription';
 
 function getMastercraftDamageDescription(&$errors, &$input) {
 	getOptionalStringParameter($errors, $input, __FILE__, MASTERCRAFT_DAMAGE_DESCRIPTION, OPTIONAL_STRING_PARAMETER);
