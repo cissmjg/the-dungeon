2321c8bda7d6a34e011d0aad303c69b5dc2d7a77
cissmjg <cissmjg@hotmail.com>
2026-01-05 15:35:19 -0500

    Corrected type with 'spellTypeId' web variable

--- webio/spellTypeId.php
+++ webio/spellTypeId.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__ . '/requiredParameter.php';
-const SPELL_TYPE_ID = 'spellRTypeId';
+const SPELL_TYPE_ID = 'spellTypeId';
 
 function getSpellTypeId(&$errors, &$input) {
 	getRequiredIntegerParameter($errors, $input, __FILE__, SPELL_TYPE_ID);
