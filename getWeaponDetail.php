1a8fb3da609d7ca714b69e6b9da507ec9b0afcde
cissmjg <cissmjg@hotmail.com>
2026-01-12 19:35:43 -0500

    Updated incorrect reference to weaponDetail class

--- getWeaponDetail.php
+++ getWeaponDetail.php
@@ -12,7 +12,7 @@ require_once __DIR__ . '/helper/RestHeaderHelper.php';
 require_once __DIR__ . '/webio/playerName.php';
 require_once __DIR__ . '/webio/characterName.php';
 require_once __DIR__ . '/webio/weaponProficiencyId.php';
-require_once 'weaponDetail.php';
+require_once __DIR__ . '/classes/weaponDetail.php';
 
 // Filter and sanitize names
 getPlayerName($errors, $input);
