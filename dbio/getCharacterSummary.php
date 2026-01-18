8fc93c011ba7de23b1e40e0902d8e7a599d6262b
cissmjg <cissmjg@hotmail.com>
2026-01-18 13:07:09 -0500

    Moved getCharacterSummary.php to /dbio/.

--- dbio/getCharacterSummary.php
+++ dbio/getCharacterSummary.php
@@ -3,15 +3,15 @@
 $errors = [];
 $input = [];
 
-require_once __DIR__ . '/validateCredentials.php';
-$pdo = require_once __DIR__ . '/dbio/DBConnection.php';
+require_once __DIR__ . '/../validateCredentials.php';
+$pdo = require_once __DIR__ . '/DBConnection.php';
 
 validateSessionCredentials($pdo);
 
-require_once __DIR__ . '/helper/RestHeaderHelper.php';
-require_once __DIR__ . '/webio/playerName.php';
-require_once __DIR__ . '/webio/characterName.php';
-require_once __DIR__ . '/classes/characterSummary.php';
+require_once __DIR__ . '/../helper/RestHeaderHelper.php';
+require_once __DIR__ . '/../webio/playerName.php';
+require_once __DIR__ . '/../webio/characterName.php';
+require_once __DIR__ . '/../classes/characterSummary.php';
 
 // Filter and sanitize names
 getPlayerName($errors, $input);
