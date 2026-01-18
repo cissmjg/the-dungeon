19fb1ba7805e6338d1cdff44aefe95b509b58307
cissmjg <cissmjg@hotmail.com>
2026-01-18 15:51:46 -0500

    Moved getPlayerCharacterWeapon.php

--- dbio/getPlayerCharacterWeapon.php
+++ dbio/getPlayerCharacterWeapon.php
@@ -3,18 +3,18 @@
 $errors = [];
 $input = [];
 
-require_once __DIR__ . '/validateCredentials.php';
-$pdo = require_once __DIR__ . '/dbio/DBConnection.php';
+require_once __DIR__ . '/../validateCredentials.php';
+$pdo = require_once __DIR__ . '/DBConnection.php';
 
 validateSessionCredentials($pdo);
 
-require_once __DIR__ . '/helper/RestHeaderHelper.php';
-require_once 'playerCharacterWeapon.php';
-require_once __DIR__ . '/webio/playerCharacterWeaponId.php';
-require_once __DIR__ . '/webio/playerName.php';
-require_once __DIR__ . '/webio/characterName.php';
-require_once __DIR__ . '/webio/weaponProficiencyId.php';
-require_once 'weaponDetail.php';
+require_once __DIR__ . '/../helper/RestHeaderHelper.php';
+require_once __DIR__ . '/../webio/playerCharacterWeaponId.php';
+require_once __DIR__ . '/../webio/playerName.php';
+require_once __DIR__ . '/../webio/characterName.php';
+
+require_once __DIR__ . '/../classes/weaponDetail.php';
+require_once __DIR__ . '/../classes/playerCharacterWeapon.php';
 
 // Filter and sanitize names
 getPlayerName($errors, $input);
