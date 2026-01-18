628b2606f22a925d30419a399ab49b3b68fae2b6
cissmjg <cissmjg@hotmail.com>
2026-01-18 12:31:44 -0500

    Moved getAvailableSkillsForPlayerCharacter.php to /dbio/.

--- dbio/getAvailableSkillsForPlayerCharacter.php
+++ dbio/getAvailableSkillsForPlayerCharacter.php
@@ -4,15 +4,15 @@ $errors = [];
 $input = [];
 $log = [];
 
-require_once __DIR__ . '/validateCredentials.php';
-$pdo = require_once __DIR__ . '/dbio/DBConnection.php';
+require_once __DIR__ . '/../validateCredentials.php';
+$pdo = require_once __DIR__ . '/DBConnection.php';
 
 validateSessionCredentials($pdo);
 
-require_once __DIR__ . '/helper/RestHeaderHelper.php';
-require_once __DIR__ . '/webio/playerName.php';
-require_once __DIR__ . '/webio/characterName.php';
-require_once __DIR__ . '/classes/availableCharacterSkills.php';
+require_once __DIR__ . '/../helper/RestHeaderHelper.php';
+require_once __DIR__ . '/../webio/playerName.php';
+require_once __DIR__ . '/../webio/characterName.php';
+require_once __DIR__ . '/../classes/availableCharacterSkills.php';
 
 // Filter and sanitize names
 getPlayerName($errors, $input);
