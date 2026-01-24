afa4f698d6c61f038a7ccc25b860db3fe92107d5
cissmjg <cissmjg@hotmail.com>
2026-01-24 18:37:33 -0500

    Moved all Javascript files to /js/. Moved all CSS files to /css/. Updated HtmlHelper to use new locations. Added latest version of jQuery UI. Updated HtmlHelper to use jQuery 3.7.1

--- helper/HtmlHelper.php
+++ helper/HtmlHelper.php
@@ -2,8 +2,8 @@
 
 class HtmlHelper {
 
-const JQUERY_LIB = "../js/jquery-1.12.4.min.js";
-const JQUERY_UI_LIB = "../js/jquery-ui.min.js";
+const JQUERY_LIB = "https://code.jquery.com/jquery-3.7.1.min.js";
+const JQUERY_UI_LIB = "js/jquery-ui.min.js";
 
 const FONT_AWESOME_LIB = "https://kit.fontawesome.com/4295d6f264.js";
 
@@ -29,21 +29,21 @@ const FONT_AWESOME_LIB = "https://kit.fontawesome.com/4295d6f264.js";
         $output_html .= '    <meta name="viewport" content="width=device-width, initial-scale=1.0">' . PHP_EOL;
         $output_html .= '    <meta name="Cache-Control" content="no-store">' . PHP_EOL . PHP_EOL;
         $output_html .= '    <title>' . $page_title . '</title>' . PHP_EOL . PHP_EOL;
-        $output_html .= '    <script src="' . HtmlHelper::JQUERY_LIB . '"></script>' . PHP_EOL;
+        $output_html .= '    <script src="' . HtmlHelper::JQUERY_LIB . '" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>' . PHP_EOL;
         $output_html .= '    <script src="' . HtmlHelper::JQUERY_UI_LIB . '"></script>' . PHP_EOL;
         $output_html .= '    <script src="' . HtmlHelper::FONT_AWESOME_LIB . '" crossorigin="anonymous"></script>' . PHP_EOL . PHP_EOL;
-        $output_html .= '    <link rel="stylesheet" href="' . $site_css_file .'">' . PHP_EOL . PHP_EOL;
+        $output_html .= '    <link rel="stylesheet" href="css/' . $site_css_file .'">' . PHP_EOL . PHP_EOL;
         if ($enable_toggle_panels) {
             $output_html .= '    <link rel="stylesheet" href="togglePanel.css">' . PHP_EOL;
-            $output_html .= '    <script type="module" src="togglePanel.js"></script>' . PHP_EOL . PHP_EOL;
+            $output_html .= '    <script type="module" src="js/togglePanel.js"></script>' . PHP_EOL . PHP_EOL;
         }
 
         if (!empty($page_specific_js)) {
-            $output_html .= '    <script src="' . $page_specific_js . '" type="module"></script>' . PHP_EOL;
+            $output_html .= '    <script src="js/' . $page_specific_js . '" type="module"></script>' . PHP_EOL;
         }
 
         if (!empty($page_specific_css)) {
-             $output_html .= '    <link href="' . $page_specific_css .'" rel="stylesheet">' . PHP_EOL;
+             $output_html .= '    <link href="css/' . $page_specific_css .'" rel="stylesheet">' . PHP_EOL;
         }
 
         $output_html .= '</head>' . PHP_EOL;
